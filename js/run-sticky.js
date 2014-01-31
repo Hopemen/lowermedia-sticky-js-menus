c5b04e7c2b832214d0f156d3c7f163e7182ea4c6
Pete Lower <pete.lower@gmail.com>
2014-01-31 16:00:43 -0600

    Remove comments, format, add top comments

--- js/run-sticky.js
+++ js/run-sticky.js
@@ -1,47 +1,52 @@
+/*
+#
+#
+#   This script recieves a parameter (the active theme's themename) from lowermedia-sticky-js-menus.php,
+#   it uses this parameter to decide which div to put the sticky wrapper around.  The .sticky function
+#   is defined in jquery.sticky.js.  This script also adds two classes to the body for debuging/testing
+#   and/or styling if needed
+#
+#
+*/
+
 jQuery(document).ready(function(){
-  //
-  //	DEFAULT
-  //
-  //jQuery(".lowermedia_add_sticky").sticky({ topSpacing: 0 });//#nav-wrapper
-  //jQuery("#topbar").sticky({ topSpacing: 0 });//#nav-wrapper
-  //document.write(LMScriptParams.themename);
   jQuery('body').addClass('pete-runsticky');
   jQuery('body').addClass(LMScriptParams.themename);
 
-  if (LMScriptParams.themename=='twentythirteen') 
-  	{
-		jQuery("#navbar").sticky({ topSpacing: 0 });//#twentythirteen
-	} 
+  if (LMScriptParams.themename=='twentythirteen')
+    {
+      jQuery("#navbar").sticky({ topSpacing: 0 });//#twentythirteen
+    }
   else if (LMScriptParams.themename=='twentytwelve')
     {
-		jQuery("#site-navigation").sticky({ topSpacing: 0 });//twentytwelve
+      jQuery("#site-navigation").sticky({ topSpacing: 0 });//twentytwelve
     }
   else if (LMScriptParams.themename=='twentyeleven')
     {
-		jQuery("nav#access").sticky({ topSpacing: 0 });//twentyeleven
+      jQuery("nav#access").sticky({ topSpacing: 0 });//twentyeleven
     }
   else if (LMScriptParams.themename=='twentyten')
     {
-		jQuery("#access").sticky({ topSpacing: 0 });//twentyten
+      jQuery("#access").sticky({ topSpacing: 0 });//twentyten
     }
   else if (LMScriptParams.themename=='responsive')
     {
-		jQuery(".main-nav").sticky({ topSpacing: 0 });//responsive
+      jQuery(".main-nav").sticky({ topSpacing: 0 });//responsive
     }
-  else if (LMScriptParams.themename=='virtue') 
-  	{
-	  	jQuery("#topbar").sticky({ topSpacing: 0 });//virtue
-	} 
-  else if (LMScriptParams.themename=='required-foundation') 
-  	{
-		jQuery("#access").sticky({ topSpacing: 0 });// required-foundation
-	}
-  else if (LMScriptParams.themename=='wp-foundation') 
-  	{
-		jQuery(".top-nav").sticky({ topSpacing: 0 });// wp-foundation
-	}
-  else 
-  	{
-		jQuery(".lowermedia_add_sticky").sticky({ topSpacing: 0 });//#default
+  else if (LMScriptParams.themename=='virtue')
+    {
+      jQuery("#topbar").sticky({ topSpacing: 0 });//virtue
+    }
+  else if (LMScriptParams.themename=='required-foundation')
+    {
+      jQuery("#access").sticky({ topSpacing: 0 });// required-foundation
+    }
+  else if (LMScriptParams.themename=='wp-foundation')
+    {
+      jQuery(".top-nav").sticky({ topSpacing: 0 });// wp-foundation
+    }
+  else
+    {
+      jQuery(".lowermedia_add_sticky").sticky({ topSpacing: 0 });//#default
     }
 });
