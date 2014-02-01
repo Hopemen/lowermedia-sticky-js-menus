ac1af741a62de6f0e154aea9293e78ee2b264260
Pete Lower <pete.lower@gmail.com>
2014-01-31 18:10:21 -0600

    Code fix

--- js/run-sticky.js
+++ js/run-sticky.js
@@ -53,13 +53,9 @@ jQuery(document).ready(function(){
     {
       jQuery(".art-nav").sticky({ topSpacing: 0 });//Swtor_NeozOne_Wp
     }
-  else if (LMScriptParams.themename=='lowermedia_one_page_theme')
+  else if (LMScriptParams.themename=='lowermedia_one_page_theme' || LMScriptParams.themename=='expound')
     {
-      jQuery("#site-navigation").sticky({ topSpacing: 0 });//lowermedia_one_page_theme
-    }
-  else if (LMScriptParams.themename=='lowermedia_one_page_theme')
-    {
-      jQuery("#site-navigation").sticky({ topSpacing: 0 });//expound
+      jQuery("#site-navigation").sticky({ topSpacing: 0 });//lowermedia_one_page_theme or expound
     }
   else
     {
