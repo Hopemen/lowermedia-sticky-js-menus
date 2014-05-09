253346d6c8e45e05eb61a6f69efad5a08e675518
Pete Lower <pete.lower@gmail.com>
2014-05-09 14:53:51 -0500

    Add spacious and Isabelle theme to php file

--- lowermedia-sticky-js-menus.php
+++ lowermedia-sticky-js-menus.php
@@ -55,6 +55,8 @@ function lowermedia_add_sticky_js()
 		&& $theme_data['Template']!='destro'
 		&& $theme_data['Template']!='attitude'
 		&& $theme_data['Template']!='spun'
+		&& $theme_data['Template']!='Isabelle'
+		&& $theme_data['Template']!='spacious'
 		&& $theme_data['Template']!='bushwick')
 	{
 		function my_wp_nav_menu_args( $args = '' )
