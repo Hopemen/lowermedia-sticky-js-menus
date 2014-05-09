ee219a825317efba257928c43a3acd65e04105f0
Pete Lower <pete.lower@gmail.com>
2014-05-09 14:51:03 -0500

    Meta mods

--- js/run-sticky.js
+++ js/run-sticky.js
@@ -2,9 +2,6 @@
 #
 #
 #   This script recieves a parameter (the active theme's themename) from lowermedia-sticky-js-menus.php,
-#   it uses this parameter to decide which div to put the sticky wrapper around.  The .sticky function
-#   is defined in jquery.sticky.js.  This script also adds two classes to the body for debuging/testing
-#   and/or styling if needed
 #   it uses this parameter to decide which div to put the sticky wrapper around.  
 #
 #   The .sticky function is defined in jquery.sticky.js.  
