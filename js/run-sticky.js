4c6d046d5488e11c1061fed2e8fb32abe923eb2c
Pete Lower <pete.lower@gmail.com>
2014-06-26 15:43:14 -0500

    Hide old code, un needed

--- js/run-sticky.js
+++ js/run-sticky.js
@@ -16,64 +16,6 @@ jQuery(document).ready(function(){
   jQuery('body').addClass(LMScriptParams.themename);
   jQuery('body').addClass(LMScriptParams.disableatwidth);
 
-  if (LMScriptParams.themename=='twentythirteen')
-    {
-      jQuery("#navbar").sticky({ topSpacing: 0 });//#twentythirteen
-    }
-  else if (LMScriptParams.themename=='twentyeleven')
-    {
-      jQuery("nav#access").sticky({ topSpacing: 0 });//twentyeleven
-    }
-  else if (LMScriptParams.themename=='twentyten')
-    {
-      jQuery("#access").sticky({ topSpacing: 0 });//twentyten
-    }
-  else if (LMScriptParams.themename=='responsive')
-    {
-      jQuery(".main-nav").sticky({ topSpacing: 0 });//responsive
-    }
-  else if (LMScriptParams.themename=='virtue')
-    {
-      jQuery("#topbar").sticky({ topSpacing: 0 });//virtue
-    }
-  else if (LMScriptParams.themename=='wp-foundation')
-    {
-      jQuery(".top-nav").sticky({ topSpacing: 0 });//wp-foundation
-    }
-  else if (LMScriptParams.themename=='neuro')
-    {
-      jQuery("#navigation_menu").sticky({ topSpacing: 0 });//neuro
-    }
-  else if (LMScriptParams.themename=='Swtor_NeozOne_Wp')
-    {
-      jQuery(".art-nav").sticky({ topSpacing: 0 });//Swtor_NeozOne_Wp
-    }
-  else if (LMScriptParams.themename=='destro')
-    {
-      jQuery("#menu").sticky({ topSpacing: 0 });//destro
-    }
-  else if (LMScriptParams.themename=='attitude' || LMScriptParams.themename=='required-foundation')
-    {
-      jQuery("#access").sticky({ topSpacing: 0 });//attitude or required-foundation
-    }
-  else if (LMScriptParams.themename=='spun')
-    {
-      jQuery(".site-navigation").sticky({ topSpacing: 0 });//spun
-    }
-  else if (LMScriptParams.themename=='Isabelle')
-    {
-      jQuery(".nav").sticky({ topSpacing: 0 });//spun
-    }
-  else if (LMScriptParams.themename=='one-page')
-    {
-      jQuery(".header_wrapper").sticky({ topSpacing: 0 });//one-page
-      jQuery(".homepage_nav_title").sticky({ topSpacing: 0 });//spun
-    }
-  else if (LMScriptParams.themename=='spacious')
-    {
-      jQuery("#header-text-nav-container").sticky({ topSpacing: 0 });//spun
-    }
-  else if (LMScriptParams.themename=='lowermedia_one_page_theme' || LMScriptParams.themename=='expound' || LMScriptParams.themename=='sixteen' || LMScriptParams.themename=='bushwick' || LMScriptParams.themename=='twentytwelve')
     {
       jQuery("#site-navigation").sticky({ topSpacing: 0 });//lowermedia_one_page_theme, expound, sixteen, bushwik, or twentytwelve
     }
